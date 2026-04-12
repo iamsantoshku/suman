053b863dfaacdc69a04ee5fffc8bba4e4cc6eb31
Santosh kumar <santoshkumar@Mac.lan>
2026-04-12 18:59:41 +0530

    add icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// const Navbar = () => {
-//   return (
-//     // <div>Navbar</div>
-//     <div className="flex justify-center gap-6 p-4 bg-pink-500 text-white">
-//       <Link to="/">Home</Link>
-//       <Link to="/gallery">Gallery</Link>
-//       <Link to="/story">Story</Link>
-//       <Link to="/message">Message</Link>
-//       <Link to="/proposal">Proposal</Link>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
-
 
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
